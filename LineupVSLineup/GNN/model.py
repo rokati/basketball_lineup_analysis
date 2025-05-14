aba726a61ad7dd90e81fff26bc8b451ec3c9d172
rokaati18 <rokaati18@edu.bme.hu>
2025-05-14 17:03:04 +0200

    Add GINConv import, update Jupyter notebook execution counts, and add team data CSV
    - Imported GINConv from torch_geometric.nn in model.py for enhanced graph neural network capabilities.
    - Updated execution counts in data_download_pbp.ipynb to reflect the latest code execution.
    - Added code to fetch and save team statistics to a CSV file for the 2023-24 season.
    - Created requirements.txt and included 'gradio' as a new dependency.
    - Implemented gradio app for predictions

--- LineupVSLineup/GNN/model.py
+++ LineupVSLineup/GNN/model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import pytorch_lightning as pl
-from torch_geometric.nn import GINEConv
+from torch_geometric.nn import GINEConv, GINConv
 from torchmetrics import Accuracy, AUROC
 
 class GNNModel(nn.Module):
